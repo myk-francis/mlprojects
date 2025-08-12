7051fcedb56f3cb935d33d12c7dda23424ccc622
myk <michaelmugendi1707@gmail.com>
2025-08-12 10:55:54 +0300

    Update: model trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,85 @@
+import os, sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import ( AdaBoostRegressor, RandomForestRegressor, GradientBoostingRegressor)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.neighbors import KNeighborsRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_path = os.path.join("artifacts", "model.pkl")
+    # model_trainer_config_path = os.path.join("artifacts", "model_trainer.yaml")
+    # model_config_path = os.path.join("artifacts", "model.yaml")
+    base_accuracy = 0.6
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Split training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            params={
+            "Decision Tree": {
+                'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                'splitter':['best','random'],
+                'max_features':['sqrt','log2'],
+            },
+            "Random Forest":{
+                # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                # 'max_features':['sqrt','log2',None],
+                'n_estimators': [8,16,32,64,128,256]
+            },
+            }
+
+            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models)
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score < self.model_trainer_config.base_accuracy:
+                raise CustomException("No best model found", "Model not found")
+            logging.info("Best model found on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
